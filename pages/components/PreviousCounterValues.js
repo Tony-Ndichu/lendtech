e31e54493be945aff422ac523c4124d8904329bd
Tony-Ndichu <ndichutony@gmail.com>
2022-06-23 09:45:26 +0300

    feat: enable pagination

--- pages/components/PreviousCounterValues.js
+++ pages/components/PreviousCounterValues.js
@@ -1,12 +1,59 @@
 import styled from "styled-components";
+import ReactPaginate from "react-paginate";
+import { useState, useEffect } from "react";
 
-const PreviousCounterValues = ({
-    counterValues
-}) => {
-    console.log("counterValues==========>", counterValues)
-    return counterValues?.map(item => {
-        return <>{item}<br /></>
-    })
-}
+const PreviousCounterValues = ({ counterValues, itemsPerPage = 5 }) => {
+  const items = counterValues;
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+  // Here we use item offsets; we could also use page offsets
+  // following the API or data you're working with.
+  const [itemOffset, setItemOffset] = useState(0);
+
+  useEffect(() => {
+    // Fetch items from another resources.
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    setCurrentItems(items.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(items.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, items]);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % items.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
+  const Items = ({ currentItems }) => {
+    return (
+      <>
+        {currentItems &&
+          currentItems.map((item) => (
+            <div>
+              <h3>{item}</h3>
+            </div>
+          ))} <br/ >
+      </>
+    );
+  };
+
+  return (
+    <>
+      <Items currentItems={currentItems} />
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+      />
+    </>
+  );
+};
 
 export default PreviousCounterValues;
